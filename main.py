97c626296719cf0d43d71ecf0f5c561805cafa42
Max <1665052137@qq.com>
2025-09-30 16:14:04 +0800

    Prompt templates

--- main.py
+++ main.py
@@ -17,33 +17,84 @@ from langgraph.checkpoint.memory import MemorySaver
 # StateGraph：状态图类，用于构建工作流
 from langgraph.graph import START, MessagesState, StateGraph
 
-# Define a new graph
+# # Define a new graph
+# workflow = StateGraph(state_schema=MessagesState)
+
+
+# # Define the function that calls the model
+# def call_model(state: MessagesState):
+#     response = model.invoke(state["messages"])
+#     return {"messages": response}
+
+
+# # Define the (single) node in the graph
+# workflow.add_edge(START, "model")
+# workflow.add_node("model", call_model)
+
+# # Add memory
+# memory = MemorySaver()
+# app = workflow.compile(checkpointer=memory)
+
+# config = {"configurable": {"thread_id": "abc123"}}
+
+# query = "Hi! I'm Bob."
+
+# input_messages = [HumanMessage(query)]
+# output = app.invoke({"messages": input_messages}, config)
+# output["messages"][-1].pretty_print()  # output contains all messages in state
+
+# query = "What's my name?"
+
+# input_messages = [HumanMessage(query)]
+# output = app.invoke({"messages": input_messages}, config)
+# output["messages"][-1].pretty_print()
+
+# config = {"configurable": {"thread_id": "abc234"}}
+
+# input_messages = [HumanMessage(query)]
+# output = app.invoke({"messages": input_messages}, config)
+# output["messages"][-1].pretty_print()
+
+# config = {"configurable": {"thread_id": "abc123"}}
+
+# input_messages = [HumanMessage(query)]
+# output = app.invoke({"messages": input_messages}, config)
+# output["messages"][-1].pretty_print()
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+
+prompt_template = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You talk like a pirate. Answer all questions to the best of your ability.",
+        ),
+        MessagesPlaceholder(variable_name="messages"),
+    ]
+)
 workflow = StateGraph(state_schema=MessagesState)
 
 
-# Define the function that calls the model
 def call_model(state: MessagesState):
-    response = model.invoke(state["messages"])
+    prompt = prompt_template.invoke(state)
+    response = model.invoke(prompt)
     return {"messages": response}
 
 
-# Define the (single) node in the graph
 workflow.add_edge(START, "model")
 workflow.add_node("model", call_model)
 
-# Add memory
 memory = MemorySaver()
 app = workflow.compile(checkpointer=memory)
 
-config = {"configurable": {"thread_id": "abc123"}}
+config = {"configurable": {"thread_id": "abc345"}}
 
-query = "Hi! I'm Bob."
+query = "Hi! I'm Jim."
 
 input_messages = [HumanMessage(query)]
 output = app.invoke({"messages": input_messages}, config)
-output["messages"][-1].pretty_print()  # output contains all messages in state
+output["messages"][-1].pretty_print()
 
-query = "What's my name?"
+query = "What is my name?"
 
 input_messages = [HumanMessage(query)]
 output = app.invoke({"messages": input_messages}, config)
